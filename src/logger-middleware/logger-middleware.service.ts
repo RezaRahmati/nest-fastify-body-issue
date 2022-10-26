2b0011a8bb496a874257c3c2dae371c978c7f4a7
Reza Rahmati <reza.rahmati@pcbank.ca>
2022-10-26 18:08:59 -0400

    add comment

--- src/logger-middleware/logger-middleware.service.ts
+++ src/logger-middleware/logger-middleware.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class LoggerMiddlewareService implements NestMiddleware {
+
+  // what should be type of request and response and next?
   use(request: any, response: any, next: any) {
     console.log('body', request.body);
     console.log('query', request.query);
